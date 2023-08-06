93e945b6c28f8fc0fe68056fff89ae330c862ec2
Musa Shaba <musamohshaba@gmail.com>
2023-08-06 14:18:47 +0100

    low Battery

---
+++ assets/scripts/test.js
@@ -0,0 +1,5 @@
+const form1 = document.getElementById('form1');
+const error1 = document.getElementById('');
+const form2 = document.getElementById('');
+const email2 = document.getElementById('');
+const error2 = document.getElementById('');
